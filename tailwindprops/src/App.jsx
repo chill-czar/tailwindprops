5b00a279bc78141e6433cc745a6070f1f8335f17
aisarthak <aisarthak2@gmail.com>
2024-10-30 08:04:29 +0530

    props passing and how to use ti
    Signed-off-by: aisarthak <aisarthak2@gmail.com>

--- tailwindprops/src/App.jsx
+++ tailwindprops/src/App.jsx
@@ -1,11 +1,14 @@
 import { useState } from 'react'
 import './App.css'
+import Card from './components/Card'
 
 function App() {
   return (
     <>
 
       <h1 className='underline bg-blue rounded-50% font-bold border '>hello world </h1>
+      <Card />
+    <Card channel="chai aur code" name ="JIMMY" />
     </>
   )
 }
